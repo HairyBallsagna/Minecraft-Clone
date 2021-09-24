8ae3f4c161be92e196086c96d56a6efa380bfaec
Lakshay <lakshayyadav2006@gmail.com>
2021-09-24 23:14:37 +0530

    added the world script and created a constructor for the ChunkData class

--- Minecraft Clone/Assets/Scripts/ChunkData.cs
+++ Minecraft Clone/Assets/Scripts/ChunkData.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChunkData : MonoBehaviour
+public class ChunkData
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public VoxelType[] voxels;
+    public int chunkSize = 16;
+    public int chunkHeight = 100;
+    public World worldRef;
+    public Vector3Int worldPos;
 
-    // Update is called once per frame
-    void Update()
+    public bool modifiedByThePlayer = false;
+
+    public ChunkData(int chunkSize, int chunkHeight, World world, Vector3Int worldPos)
     {
-        
+        this.chunkSize = chunkSize;
+        this.chunkHeight = chunkHeight;
+        this.worldRef = world;
+        this.worldPos = worldPos;
+        voxels = new VoxelType[chunkSize * chunkHeight * chunkSize];
     }
 }
